b3355f495e9d23337bb008cb738a944581b36d1b
Max <roosmaxm@arcada.fi>
2024-04-09 09:00:36 +0000

    Lektion 5

--- hotel-cc/app.py
+++ hotel-cc/app.py
@@ -2,51 +2,71 @@ import os, psycopg
 from psycopg.rows import dict_row
 from flask import Flask, request
 from flask_cors import CORS
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
+
+# pip install psycopg_binary python-dotenv
 
 load_dotenv()
 
 PORT=8300 
 
 db_url = os.environ.get("DB_URL")
-print(db_url)
+print(os.environ.get("FOO"))
 
-conn = psycopg.connect(db_url, autocommit=True, row_factory=dict_row)
+conn = psycopg.connect(db_url, row_factory=dict_row)
 
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
 
-rooms = [
+roomsTEMP = [
     { 'number': 101, 'type': "single" },
     { 'number': 202, 'type': "double" },
     { 'number': 303, 'type': "suite" }
 ]
 
-@app.route("/test", )
-def dbtest():
-     with conn.cursor() as cur:
-        cur.execute("SELECT * from people")
-        rows = cur.fetchall()
-        return rows
-    
+@app.route("/", )
+def info():
+    #return "<h1>Hello, Flask!</h1>"
+    return "Hotel API, endpoints /rooms, /bookings"
+
 
 @app.route("/rooms", methods=['GET', 'POST'])
 def rooms_endoint():
     if request.method == 'POST':
         request_body = request.get_json()
         print(request_body)
-        rooms.append(request_body)
+        roomsTEMP.append(request_body)
         return { 
-            'msg': f"Du har skapat ett nytt rum, id: {len(rooms)-1}!"
+            'msg': f"Du har skapat ett nytt rum, id: {len(roomsTEMP)-1}!"
         }
     else:
-        return rooms
+        with conn.cursor() as cur:
+            cur.execute("""SELECT * 
+                        FROM hotel_room 
+                        ORDER BY room_number""")
+            return cur.fetchall()
 
 @app.route("/rooms/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'] )
 def one_room_endpoint(id):
         if request.method == 'GET':
-            return rooms[id]
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT * 
+                    FROM hotel_room 
+                    WHERE id = %s""", [id])
+
+                return cur.fetchone()
         
+@app.route("/bookings", methods=['GET', 'POST'])
+def bookings():
+    if request.method == 'POST':
+        request_body = request.get_json()
+        print(request_body)
+        # Skapa rad i hotel_booking med sql INSERT INTO...
+        return { 
+            "msg": "APIN svarar!", 
+            "request_body": request_body 
+        }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
